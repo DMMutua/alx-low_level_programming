a9b1e0a2f653646c7fa9cf73014b185e3e67cff1
DMMutua <davymtuerr@gmail.com>
2022-07-04 15:47:11 -0700

    Combination of Numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a Series of Single digit Numbers
+ *
+ * Return: Always (Success);
+ */
+int main(void)
+{
+	int s;
+
+	for (s = '0'; s <= '9'; c++)
+	{
+		putchar(c);
+
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
