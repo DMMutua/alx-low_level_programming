4a6656a05c20add1fd5d174db8218199b89ba00c
DMMutua <davymtuerr@gmail.com>
2022-07-05 10:37:57 -0700

    Checking for lowercase letters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase characters or otherwise
+ *
+ * Return: 1 incase of a lowercase and 0 for otherwise
+ */
+int _islower(int c);
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+
+	return (0);
+}
