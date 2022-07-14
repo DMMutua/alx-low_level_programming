c3cdd4540996546835f841f85cd253091700306e
DMMutua <davymtuerr@gmail.com>
2022-07-14 13:55:00 -0700

    Changes string lowercases to uppercases

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * string_toupper - Changes all string lowers to uppers
+ * @p: The string to be modified
+ * 
+ * Return: A string of characters
+ */
+
+char *string_toupper(char *p)
+{
+	int a = 0;
+
+	while (p[a])
+	{
+		if (p[a] >= 97 && p[a] <= 122)
+		{
+			p[a] -= 32;
+		}
+
+		a++
+	}
+
+	return (p);
+}
