d370982edbe499c076b267ab035424ffd2645d29
DMMutua <davymtuerr@gmail.com>
2022-07-18 12:39:41 -0700

    a script to implement memset manually

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,2 @@
+VimCrypt~03!ìµ5Z.HÇÑp®R-”¶S:­*¾;Ügµ¸/¶×€›[”6ÁXƒAJ®ðïÔãÈö=‰-^ƒÉ,óáOø5Z€½5¸&—dÞ0)%R,u+*"ªž•§í¹å½Àÿ¸…á$Z»ƒåfÓ“!ªèÈ•&¿ƒümÕý‚â˜Á—AËÖã½…â¤ CŸÐy´„€`_èƒÂ¸·f!U‰r‘ôÞaüƒPñãí•™\ðÛ°§Iy·?%àñ|Ùš@8Ñ,PŠgëçÎ“v! gº:ß’ù¼{vµ4Ê§i€U«’Ú\
+3d¡ŒÞË;„E™bcþ4··/M0 õÓ~Cv õG~ä9/Ì¤Wkä.O†þ©DeÒY¶ö@=ìì×ëÚÓÁÏÉuùÑÔØ+‘ÔÃ.J;@*ƒîºé£[(Öœç%²
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This section provides more scripts that demonstrate concepts of pointers, pointer-to-pointer, arrays, and multi-dimensional arrays in C language. some of the scripts seek to manually remake some global functions
