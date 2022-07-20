78107b0ae0bcf2ce4e89ad98ab0d9ddbb1cf4786
DMMutua <davymtuerr@gmail.com>
2022-07-20 14:35:10 -0700

    Factorial calculation recursively

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+int factorial(int n);
+{
+	int res;
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	if (n <= 1)
+	{
+		return (n);
+	}
+
+	return (n * factorial(n - 1));
+}
