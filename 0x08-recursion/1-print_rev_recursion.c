aeb47ad9c0196ef6821455a6571d41a381a3f8ab
DMMutua <davymtuerr@gmail.com>
2022-07-20 13:47:26 -0700

    Recursively reversing a string

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - Prints a string in reverse order 
+ * _print_rev_recursion - Prints a string in reverse order
  * recursively followed by a new line
  * @s: the pointer to a string in question
  *
@@ -10,7 +10,7 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if ( *s != '\0')
+	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
 		putchar(*s);
