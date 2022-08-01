af7e3f1af796acaa210597da807e2ba92a42368f
DMMutua <davymtuerr@gmail.com>
2022-08-01 06:01:13 -0700

    initializing a variable of a type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifdef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - a dog basic info
+ * @name: First member
+ * @age: Second structure member
+ * @owner: Third STRUCTURE member
+ *
+ * Description: Long
+ */
 struct dog
 {
 	char *name;
@@ -8,6 +16,9 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - typedef for struct dog
+ */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, *owner);
