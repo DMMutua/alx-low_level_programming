1a9539f610ec0ee079b5ba2a37db97213f8340d3
DMMutua <davymtuerr@gmail.com>
2022-08-02 05:24:23 -0700

    printing filename using macro

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * main - prints the name of the file the function was compiled
+ * from
+ *
+ * Return: 0
+ */
+
 int main(void)
 {
 	printf("%s\n", _FILE_);
