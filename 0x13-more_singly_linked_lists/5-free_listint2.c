20ad2ae8a4d0e695485259f9a2a5c630bfd01d24
DMMutua <davymtuerr@gmail.com>
2022-08-15 05:48:59 -0700

    Task 5 done and dusted

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees memory occupied by a linked list and sets
+ * the head to null
+ * @head: The head of the list in question
+ *
+ * Return: Void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head)
+	{
+		while (*head)
+		{
+			temp = (*head);
+			*head = (*head)->next;
+			free(temp);
+		}
+	}
+	else
+	{
+		return;
+	}
+
+	free(*head);
+	head = 0;
+}
