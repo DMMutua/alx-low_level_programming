954147b6a512937cf61cfe553ceb34d9ba4ca0bf
DMMutua <davymtuerr@gmail.com>
2022-08-22 06:53:16 -0700

    Task 1 complete

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file and adds some content to it
+ * @filename: the name of the file to be created
+ * @text_content: The content to add to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int op;
+	int i = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	op = open(filename, O_TRUNC | O_CREAT | O_RDWR, 0600);
+
+	if (op == -1)
+		return (-1);
+
+	while (text_content[i])
+	{
+		i++;
+	}
+
+	if (text_content != NULL)
+		write(op, text_content, i);
+
+	if (close(op) == -1)
+		return (-1);
+
+	close(op);
+	return (1);
+}
