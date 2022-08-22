41f67f35fdcf795108cb8fc89eef4b355bf8702b
DMMutua <davymtuerr@gmail.com>
2022-08-22 06:59:11 -0700

    Task 1 complete

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	op = open(filename, O_TRUNC | O_CREAT | O_RDWR, 0600);
+	op = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (op == -1)
 		return (-1);
