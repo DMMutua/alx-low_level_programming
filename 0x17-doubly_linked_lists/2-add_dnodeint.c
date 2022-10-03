5e9fddc96ba6be2038d165de632447ed807a644d
DMMutua <davymtuerr@gmail.com>
2022-10-03 16:46:27 +0300

    adding node to DL-list head

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds a new node at the beginning
+ * of a dlistint_t list
+ *
+ * @head: head of the list
+ * @n: value of the element to fill in node
+ *
+ * Return: the address of the new element
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+	dlistint_t *h;
+
+	/*Dynamic Memory Allocation for new node*/
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	/*data-filling the new node*/
+	new_node->n = n;
+	new_node->prev = NULL;
+	current_header = *head;
+
+	/*Rechecking the head points to the origin node of DL-list */
+	if (current_header != NULL)
+	{
+		while (current_header->prev != NULL)
+			current_header = current_header->prev;
+	}
+
+	/*making new_node the first node*/
+	new_node->next = current_header;
+
+	/*Adding the back-reference of new to the previous first node*/
+	if (current_header != NULL)
+		current_header->prev = new_node;
+
+	/*updating the list head to point to 'new'*/
+	*head = new_node;
+
+	return (new_node);
+}
+
+
+
