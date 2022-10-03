a7738c764c898a7e6f72c42496c751561d15dbac
DMMutua <davymtuerr@gmail.com>
2022-10-03 18:32:14 +0300

    Adding the sum of all data elements in list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - Sum of all data (n) in a doubly linked list
+  * @head: The head of the doubly linked list
+  *
+  * Return: The sum of all data
+  */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *current_node = head;
+	int list_data_sum = 0;
+
+	if (head)
+	{
+		while (current_node != NULL)
+		{
+			list_data_sum += current_node->n;
+			current_node = current_node->next;
+		}
+	}
+
+	return (list_data_sum)
+}
