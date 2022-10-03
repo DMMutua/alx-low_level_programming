ba7fa9234733e8e19ca93d785e88206c5e3a34ce
DMMutua <davymtuerr@gmail.com>
2022-10-03 16:49:04 +0300

    adding node to DL-list head

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,7 +12,7 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
-	dlistint_t *h;
+	dlistint_t *current_header;
 
 	/*Dynamic Memory Allocation for new node*/
 	new_node = malloc(sizeof(dlistint_t));
