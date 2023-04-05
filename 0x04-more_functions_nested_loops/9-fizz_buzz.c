f8a5e7882d00a826a7622f66cbe80b615ae53fc4
DMMutua <lengowriter99@gmail.com>
2023-04-05 19:57:27 +0300

    the fizzbuzz challenge completed!

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ * main - A function that completes the fizzbuzz test
+ *
+ * return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if ((num % 3) == 0 && (num % 5) == 0)
+			printf("FizzBuzz");
+
+		else if ((num % 3) == 0)
+			printf("Fizz");
+		
+		else if ((num % 5) == 0)
+			printf("Buzz");
+
+		else
+			printf("%d", num);
+
+		if (num == 100)
+			continue;
+
+		printf(" ");
+	}
+
+	printf("\n");
+
+	return (0);
+}
